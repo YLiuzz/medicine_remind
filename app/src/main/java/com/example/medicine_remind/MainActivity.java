137269febc57ebb2997876fe61ec43cc0a60de17
user <tarry5508@gmail.com>
2020-01-02 17:12:23 +0800

    按鈕換頁

--- app/src/main/java/com/example/medicine_remind/MainActivity.java
+++ app/src/main/java/com/example/medicine_remind/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private  Button btnRecord;
+    private  Button btnToday;
+    private  Button btnProgress;
+    private  Button btnTeam;
+    private  Button btnMytreatment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
         btnTeam = (Button)findViewById(R.id.btn_team);
         btnMytreatment = (Button)findViewById(R.id.btn_my_treatment);
 
+        btnRecord.setOnClickListener(btnListener);
+        btnToday.setOnClickListener(btnListener);
+        btnProgress.setOnClickListener(btnListener);
+        btnTeam.setOnClickListener(btnListener);
+        btnMytreatment.setOnClickListener(btnListener);
+
+
+
+
     }
+    private View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_today:
+                    Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_progress:
+                    Intent intent1 = new Intent(MainActivity.this,progress.class);
+                    startActivity(intent1);
+            }
+        }
+    };
 
 
 
