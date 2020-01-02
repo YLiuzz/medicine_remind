330439a9ae192636e9aba7d7e6d46aa41d1a9741
user <tarry5508@gmail.com>
2020-01-02 15:46:45 +0800

    按鈕

--- app/src/main/java/com/example/medicine_remind/MainActivity.java
+++ app/src/main/java/com/example/medicine_remind/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.medicine_remind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate((savedInstanceState));
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+        btnRecord = (Button)findViewById(R.id.btn_record);
+        btnToday = (Button)findViewById(R.id.btn_today);
+        btnProgress = (Button)findViewById(R.id.btn_progress);
+        btnTeam = (Button)findViewById(R.id.btn_team);
+        btnMytreatment = (Button)findViewById(R.id.btn_my_treatment);
 
     }
+
+
+
 
 }
---
+++ app/src/main/java/com/example/medicine_remind/progress2.java
@@ -0,0 +1,4 @@
+package com.example.medicine_remind;
+
+class progress2 {
+}
